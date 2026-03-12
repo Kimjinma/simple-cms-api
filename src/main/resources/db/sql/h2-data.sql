95aaf4456edde0e1b12913ca215d33c01b046b71
Kimjinma <akflzk33@naver.com>
2026-03-12 13:49:01 +0900

    feat: 초기 더미 데이터를 세팅. 서버 구동 시 data.sql을 통해 아래의 테스트 계정과 15개의 페이징 테스트용 게시글이 자동 생성

--- src/main/resources/db/sql/h2-data.sql
+++ src/main/resources/db/sql/h2-data.sql
@@ -1,2 +1,17 @@
 -- TODO data
-
+INSERT INTO contents (title, description, view_count, created_date, created_by) VALUES
+('admin test1', 'test1', 0, CURRENT_TIMESTAMP, 'admin'),
+('user1 test2', 'test2', 0, CURRENT_TIMESTAMP, 'user1'),
+('admin test3', 'test3', 0, CURRENT_TIMESTAMP, 'admin'),
+('user1 test4', 'test4', 0, CURRENT_TIMESTAMP, 'user1'),
+('user1 test5', 'test5', 0, CURRENT_TIMESTAMP, 'user1'),
+('test6', 'test6', 0, CURRENT_TIMESTAMP, 'user1'),
+('test7', 'test7', 0, CURRENT_TIMESTAMP, 'admin'),
+('test8', 'test8', 0, CURRENT_TIMESTAMP, 'user1'),
+('test9', 'test9', 0, CURRENT_TIMESTAMP, 'user1'),
+('test10', 'test10', 0, CURRENT_TIMESTAMP, 'user1'),
+('test11', 'test11', 0, CURRENT_TIMESTAMP, 'admin'),
+('test12', 'test12', 0, CURRENT_TIMESTAMP, 'user1'),
+('test13', 'test13', 0, CURRENT_TIMESTAMP, 'user1'),
+('test14', 'test14', 0, CURRENT_TIMESTAMP, 'user1'),
+('test15', 'test15', 0, CURRENT_TIMESTAMP, 'admin');
